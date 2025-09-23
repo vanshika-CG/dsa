726d4a435e9c34b43ec40dc8ac71aa84586c83c1
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-09-23 14:24:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (63.25%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int sum =0;
-        for(int i=0 ;i<s.length()-1;i++){
-            int diff = abs(s[i+1]-s[i]);
-            sum += diff;
+        int sum = 0;
+        for(int i=0 ; i<s.length()-1; i++){
+          int diff = abs(s[i+1]-s[i]);
+          sum+=diff;
         }
         return sum;
     }
