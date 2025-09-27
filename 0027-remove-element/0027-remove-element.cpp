44d480b7cb9604c54fafa077a762609cf523f7fa
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-09-27 11:42:50 +0530

    Time: 0 ms (100%), Space: 11.8 MB (10.1%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> ans;
+        int count =0;
+        for(int i=0 ; i<nums.size(); i++){
+            if(nums[i]!=val){
+                ans.push_back(nums[i]);
+                count++;
+            }
+          
+        }
+        nums= ans;
+        return count;
+    }
+};
