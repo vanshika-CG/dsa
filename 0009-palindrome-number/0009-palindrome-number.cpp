47ccb5b938a183fac8e377d2a6d247e0869ab6f4
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-10-05 21:23:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are never palindrome
+        if (x < 0) return false;
+
+        int reversed = 0;
+        int original = x;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return reversed == original;
+    }
+};
