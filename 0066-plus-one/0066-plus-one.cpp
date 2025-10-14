75ae6de64ecaf3ca128ad8fccfe8b29c2550dddd
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-10-14 11:28:03 +0530

    Time: 0 ms (100%), Space: 11.5 MB (47.96%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        for(int i=n-1; i>=0; i--){
-            if(digits[i]<9){
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
-        
+
             digits[i] = 0;
-            
-            
         }
-        digits.insert(digits.begin() , 1);
-    return digits;
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
