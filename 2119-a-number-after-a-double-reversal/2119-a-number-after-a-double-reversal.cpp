04db24a5d218629a879239de64fbbc9dad1c5bb2
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-10-15 10:09:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (23.63%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int rev;
         if(num==0) return true;
-        while(num!=0){
-            rev = num % 10;
-            if(rev == 0){
-                return false;
-            }
-            else{
-                break;
-            }
-            num = num/10;
-        }
+        if(num%10==0) return false;
         return true;
     }
 };
