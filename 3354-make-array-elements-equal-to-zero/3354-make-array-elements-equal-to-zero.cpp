557a4104d4f5f77525dcd48ab6e2884052f882ce
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-10-29 10:58:03 +0530

    Time: 4 ms (68.57%), Space: 21.8 MB (99.52%) - LeetHub

---
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        int rightSum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
+                    ans++;
+                }
+                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
+                    ans++;
+                }
+            } else {
+                leftSum += nums[i];
+                rightSum -= nums[i];
+            }
+        }
+        return ans;
+    }
+};
