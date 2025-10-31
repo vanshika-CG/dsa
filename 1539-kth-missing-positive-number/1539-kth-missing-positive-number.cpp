e7643ab44aee54804d337bc7ab3df478824ff5ae
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-10-31 23:07:38 +0530

    Time: 0 ms (100%), Space: 13.1 MB (99.62%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -3,9 +3,13 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         int i = 0;
         int current = 1;
-        while(k >= 0){
-            if(i < arr.size() && arr[i]==current) current++;
-            else k--;
+        while(k > 0){
+            if(i < arr.size() && arr[i]==current) i++;
+            else {
+                k--;
+            if (k == 0) return current;
+
+            }
             current ++;
         }
         return current;
