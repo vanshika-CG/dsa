be394fa17c81a90f1b3df29bd8f34dbd5051b79b
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-05 21:11:34 +0530

    Time: 0 ms (100%), Space: 8.7 MB (90.21%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,10 +3,9 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.length();
         int count =0;
-        
         for(int i=n-1 ; i>=0 ; i--){
-            if(count > 0 && s[i]==' ') break;
-            if(s[i]==' ') continue; 
+            if(count>0 && s[i]==' ') break;
+            if(s[i]==' ') continue;
             else{
                 count++;
             }
