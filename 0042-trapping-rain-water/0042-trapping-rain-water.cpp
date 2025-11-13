b16fa650271f69a716ef621c5008077215ef36ee
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-13 16:38:35 +0530

    Time: 0 ms (100%), Space: 27.2 MB (30.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if (n == 0) return 0;
+        if (n == 0)
+            return 0;
         vector<int> left;
+        left.resize(n);
         vector<int> right;
+        right.resize(n);
         int l = 0, r = 0;
         for (int i = 0; i < n; i++) {
             if (l < height[i]) {
                 l = height[i];
             }
-            left.push_back(l);
+            left[i] = l;
         }
         for (int i = n - 1; i >= 0; i--) {
             if (r < height[i]) {
                 r = height[i];
             }
-            right.push_back(r);
+            right[i] = r;
         }
-        reverse(right.begin(), right.end());
 
         int sum = 0;
         for (int i = 0; i < n; i++) {
@@ -26,5 +28,4 @@ public:
         }
         return sum;
     }
-
 };
