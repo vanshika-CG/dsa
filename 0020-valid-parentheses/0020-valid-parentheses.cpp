aec13599e8f3ceee64a522d073611c36b9c9377d
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-14 14:48:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+                else{
+            if (stack.empty())
+                return false;
+            if (c == '}' && stack.top() == '{')
+                stack.pop();
+            else if (c == ')' && stack.top() == '(')
+                stack.pop();
+            else if (c == ']' && stack.top() == '[')
+                stack.pop();
+                else return false;
+                }
+        }
+        return stack.empty();
+    }
+};
