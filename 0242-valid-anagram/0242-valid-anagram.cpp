3f3040077f7fa2d512b842cc46a9d415c8a1a403
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-25 22:05:57 +0530

    Time: 1 ms (59.02%), Space: 10 MB (10.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(), t.end());
-        if(s==t) return true;
-
-        return false;
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> m;
+        for(char c : s) m[c]++;
+        for(char c : t) m[c]--;
+        for(auto p : m){
+            if(p.second != 0) return false;
+        }
+        return true;
     }
 };
