566dd0d73e531a11e2d0cd262cde20498a498f36
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-26 14:38:38 +0530

    Time: 0 ms (100%), Space: 15.9 MB (5.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
         vector<int> ans;
+        unordered_map<int,int> m;
 
-        for(int i=0; i<nums.size(); i++){
-            int first  = nums[i];
-            int  second = target - first;
-            if(m.find(second) != m.end()){
+        for(int i=0 ; i<nums.size(); i++){
+            int sec = target - nums[i];
+            if(m.find(sec) != m.end()){
                 ans.push_back(i);
-                ans.push_back(m[second]);
+                ans.push_back(m[sec]);
             }
-            m[first] = i;
+            m[nums[i]] = i;
         }
         return ans;
     }
