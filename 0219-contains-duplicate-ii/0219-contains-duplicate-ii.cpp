08907ca87151fe171a9ee6a6e3598f4ed92fa9c4
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-27 09:37:35 +0530

    Time: 66 ms (92.39%), Space: 98.6 MB (51.58%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> m;
-        
-        for(int i=0 ; i<nums.size(); i++){
-            int num = nums[i];
+        for(int i=0; i<nums.size(); i++){
+            int num =  nums[i];
             if(m.find(num) != m.end()){
                 int diff = abs(i - m[num]);
                 if(diff <= k) return true;
             }
-            m[num] =i;
+            m[num] = i;
         }
         return false;
     }
