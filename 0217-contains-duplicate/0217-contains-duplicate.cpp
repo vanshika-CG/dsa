35cd8f365bcc8d4595295cbb29cf402723db082a
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-11-28 09:29:49 +0530

    Time: 71 ms (17.88%), Space: 93 MB (14.29%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> m;
-        for(int x : nums){
-            if(m.find(x) != m.end()) return true;
-            m[x];
+        for(int i=0; i<nums.size(); i++) m[nums[i]]++;
+        for(auto &p : m){
+            if(p.second > 1) return true;
         }
         return false;
     }
