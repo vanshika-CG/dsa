454b6cd80e0fd3daba5377a03218ec8e569b0e0d
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-12-26 18:59:07 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.72%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans =0; 
+        int ans  = 0;
         for(int num : nums){
             ans = ans ^ num;
         }
