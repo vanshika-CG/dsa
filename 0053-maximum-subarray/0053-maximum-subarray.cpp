184579fdbf6bc137a77989723d67793bd9ff7f8d
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2025-12-28 13:29:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int maxi = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
+        int n = nums.size();
+        int sum =0; 
+        int maxsum = INT_MIN;
+
+        for(int i=0; i<n; i++){
             sum += nums[i];
-            maxi = max(sum, maxi);
             if(sum < 0) sum = 0;
+            maxsum = max(maxsum , sum);
         }
-        return maxi;
+        return maxsum;
     }
 };
