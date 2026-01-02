b4552176e52790b62e1fa0358df76fa854a89d4e
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-02 10:57:15 +0530

    Time: 0 ms (100%), Space: 9.6 MB (77.25%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        map<char , int> m;
-
-        char c = 'a';
-        for(int i = 26 ; i>0 ; i--){
-            m[c] = i;
-            c++;
-        }
-
         int sum = 0;
 
-        for(int i=0 ; i<s.size(); i++){
-            char c = s[i];
-            sum +=  (i + 1) * m[c]; 
+        for(int i=0; i<s.size();i++){
+            sum += (i+1) * (26 - (s[i] - 'a'));
         }
+
         return sum;
     }
 };
