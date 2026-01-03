a85cfadb2526b0a84223935ea99cf192d8d6a067
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-03 11:05:02 +0530

    Time: 14 ms (21.47%), Space: 7.7 MB (87.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        int a=0, b=1;
-        for(int i=2; i<=n ; i++){
-            int sum = a+b;
-            a = b;
-            b=sum;
-        }
-        return b;
+        if (n == 1 || n == 0)
+            return n;
+        return fib(n - 1) + fib(n - 2);
     }
 };
