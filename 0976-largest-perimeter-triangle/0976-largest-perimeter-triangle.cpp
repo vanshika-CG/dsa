7a4d61645d725eaff06c68a23cbc2e2cff76f2be
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-05 15:30:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin() , nums.end());
+        int r = nums.size() - 1;
+
+        for(int i = r ; i >= 2 ; i--){
+            if(nums[i] < nums[i-1] + nums[i-2]){
+                ans = nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return ans;
+    }
+};
