e3a22f15b7e6336e3a3bdfcd7c121bf3e805c6bb
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-20 09:49:18 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(t.size() != s.size()) return false;
-        unordered_map<int,int> m;
-        for(char x : s) m[x]++;
-        for(char x : t) m[x]--;
-        for(auto& p :m){
-            if(p.second != 0) return false;
+        unordered_map <char, int> m;
+
+        for(char c : s) m[c]++;
+        for(char c : t) m[c]--;
+
+        for(auto &p : m){
+            if(p.second) return false;
         }
         return true;
     }
