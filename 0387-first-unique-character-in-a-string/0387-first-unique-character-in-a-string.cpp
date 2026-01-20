068b56a1f68f5f4df027570ff7f17859ea558f7f
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-20 10:08:53 +0530

    Time: 6 ms (64.74%), Space: 14.9 MB (19.48%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -4,10 +4,9 @@ public:
         unordered_map<char , int> m;
 
         for(char c : s) m[c]++;
-        
-        for(int i=0; i<s.size(); i++){
-            char c = s[i];
-            if(m[c] == 1) return i;
+
+        for(int i =0 ; i < s.size() ; i++){
+            if(m[s[i]] == 1) return i;
         }
         return -1;
     }
