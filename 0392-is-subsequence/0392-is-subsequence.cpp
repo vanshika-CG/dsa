1cbb5ad8593100728687064245cb0e3ff68403b9
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-20 09:59:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (89.87%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i =0;
-        int j =0;
-        while(i<s.length() && j<t.length()){
-            if(s[i]==t[j]){
+        int i = 0 , j = 0;
+        int n = s.size() , m = t.size();
+        if(n == 0) return true;
+        while(i < n && j < m){
+            if(s[i] == t[j]){
                 i++;
+                j++;
+                if(i == n)return true;
             }
-            j++;
+             
+            else j++;
         }
-        return i==s.length();
+        return false;
     }
 };
