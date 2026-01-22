c01cab64a09379ea08add99beb53fe4f56eaec19
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-22 13:49:44 +0530

    Time: 0 ms (100%), Space: 13.6 MB (79.24%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count = 0 ;
+
+        for(int num : nums){
+            if(num < 0) count++;
+            if(num == 0) return 0;
+        }
+        if(count % 2 == 0) return 1;
+        return -1;
+    }
+};
