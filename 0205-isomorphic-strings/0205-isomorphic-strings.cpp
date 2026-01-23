66e700b39d7ba86653d6f606a64d87745be5b93e
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-23 09:53:52 +0530

    Time: 3 ms (40.21%), Space: 9.2 MB (80.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mp1; // s -> t
-        unordered_map<char, char> mp2; // t -> s
+        unordered_map<char, char> ms;
+        unordered_map<char, char> mt;
 
         for (int i = 0; i < s.size(); i++) {
-            char c1 = s[i];
-            char c2 = t[i];
 
-            if (mp1.count(c1) && mp1[c1] != c2)
+            if (ms[s[i]] && ms[s[i]] != t[i])
                 return false;
-
-            if (mp2.count(c2) && mp2[c2] != c1)
+            if (mt[t[i]] && mt[t[i]] != s[i])
                 return false;
-
-            mp1[c1] = c2;
-            mp2[c2] = c1;
+            ms[s[i]] = t[i];
+            mt[t[i]] = s[i];
         }
+
         return true;
     }
 };
