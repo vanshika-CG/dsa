a17e04d60d2f5be2fbf4bedde976bffa3c777bb6
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-23 10:47:12 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low =0;
+        int high = nums.size() - 1;
+        int low = 0;
         int mid = 0;
-        int high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 1) mid++;
-            else if(nums[mid] == 0){
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
                 swap(nums[mid], nums[low]);
-                mid++;
                 low++;
-            }
-            else{
-                swap(nums[high] , nums[mid]);
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
                 high--;
+            }else{
+                mid++;
             }
         }
-       
     }
 };
