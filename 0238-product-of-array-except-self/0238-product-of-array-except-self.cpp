bb05ddde2ab79aa849b981689aacb88843b41c72
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-23 11:27:30 +0530

    Time: 0 ms (100%), Space: 40.1 MB (79.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,14 +5,15 @@ public:
         vector<int> ans(n);
         int prefix = 1;
 
-        for(int i=0; i<n;i++){
-            ans[i] = prefix;
-            prefix *= nums[i];
+        for(int i = 0 ; i < n ; i++){
+              ans[i] = prefix;
+              prefix *= nums[i];
         }
-        int suffix =1;
-        for(int i=n-1; i>=0; i--){
-            ans[i] *= suffix;
-            suffix *=  nums[i];
+
+        int suffix = 1;
+        for(int i = n-1 ; i >= 0 ; i--){
+            ans[i] *=  suffix;
+            suffix *= nums[i];
         }
         return ans;
     }
