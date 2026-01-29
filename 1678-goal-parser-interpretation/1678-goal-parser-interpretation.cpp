6540d45ae66b47ec2ad0fd702f286f19bda2f809
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-01-29 10:25:00 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.69%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string s = "";
+
+        for (int i = 0; i < command.size(); i++) {
+            if (command[i] == 'G')
+                s += 'G';
+            else if (command[i] == '(' && command[i + 1] == ')')
+                s += 'o';
+            else if (command[i] == '(' && command[i + 1] == 'a')
+                s += "al";
+        }
+        return s;
+    }
+};
