7878501dcdba51707c976ffa40c52d54107adee2
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-02-03 10:24:39 +0530

    Time: 0 ms (100%), Space: 27.9 MB (10.15%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
         int n = nums.size();
-        if(n <= 3) return false;
-        int p = 0;
-        int q = 0;
+        if (n <= 3)
+            return false;
+        int i = 0;
 
-        if(nums[0] > nums[1]) return false;
-        
-        for(int i = 1 ; i <nums.size() -1; i++){
-            if(nums[i] > nums[i - 1] && nums[i] > nums[i+1]){
-                if(p != 0) return false;
-                 p = i;
-            } 
-             if(nums[i] < nums[i-1] && nums[i] < nums[i+1] && p != 0 && q == 0){
-                 if(q != 0) return false;
-                q = i;
-            } 
-             if(nums[i] == nums[i+1] || nums[i] == nums[i-1]) return false;
-        }
-         
-        if(p >= q) return false;
+        while (i + 1 < n && nums[i] < nums[i + 1])
+            i++;
 
-        while(p < q){
-            if(nums[p] > nums[q]) q--;
-            else return false;
-        }
-        return true;
+        if (i == 0)
+            return false;
+
+        int peak = i;
+
+        while (i + 1 < n && nums[i] > nums[i + 1])
+            i++;
+
+        if (peak == i)
+            return false;
+
+        int down = i;
+
+        while (i + 1 < n && nums[i] < nums[i + 1])
+            i++;
+
+        if (i == down)
+            return false;
+
+        return i == n - 1;
     }
 };
