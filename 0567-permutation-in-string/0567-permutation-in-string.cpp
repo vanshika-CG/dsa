ff4eda657bfee7e99386e3acb839ced814106774
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-14 10:36:51 +0530

    Time: 1 ms (74.28%), Space: 10 MB (69.81%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for (char c : s1)
+            freq1[c - 'a']++;
+
+        int n = s1.size();
+
+        for (int i = 0; i < s2.size(); i++) {
+            freq2[s2[i] - 'a']++;
+
+            if (i >= n) {
+                freq2[(s2[i - n]) - 'a']--;
+            }
+
+            if (freq1 == freq2)
+                return true;
+        }
+        return false;
+    }
+};
