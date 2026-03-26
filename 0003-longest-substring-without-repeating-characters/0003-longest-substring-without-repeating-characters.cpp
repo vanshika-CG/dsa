17734351b0a3f0a8875d5a62960abb8a27c75ce6
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-26 15:51:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int maxlen = 0;
+        int len = 0;
 
         for (int i = 0; i < n; i++) {
-            unordered_set<char> m;
+            unordered_set<int> st;
             for (int j = i; j < n; j++) {
-                if (m.count(s[j]))
+                if (st.count(s[j]))
                     break;
 
-                m.insert(s[j]);
-                maxlen = max(maxlen, j - i + 1);
+                st.insert(s[j]);
+                len = max(len, j - i + 1);
             }
         }
-        return maxlen;
+        return len;
     }
 };
