ba80899a9fab3a591d918efa930f6c57110d6bfa
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-27 13:18:17 +0530

    Time: 3 ms (45.08%), Space: 8.5 MB (64.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers are never palindrome
-        if (x < 0) return false;
+        if (x < 0)
+            return false;
 
-       long reversed = 0;
-        int original = x;
+        int num = x;
+        long long a = 0;
 
-        while (x != 0) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x /= 10;
+        while (num) {
+            int d = num % 10;
+            a = (a * 10) + d;
+            num /= 10;
         }
-
-        return reversed == original;
+        if (a == x)
+            return true;
+        return false;
     }
 };
