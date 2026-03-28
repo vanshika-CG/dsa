9bb75d421d93e23b0a063f422ff3b4e635e8c762
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-28 10:08:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int n[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string s[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int i =0;
-         string str = ""; 
-        while(num>0){
-            if(num>=n[i]){
-                str=str+s[i];
-                num = num-n[i];
-            }
-            else{
+        int n[] = {1000, 900, 500, 400, 100, 90 , 50 , 40, 10, 9, 5, 4, 1};
+        string s[] = {"M", "CM", "D", "CD", "C", "XC" ,"L" ,"XL",
+                      "X", "IX", "V", "IV", "I"};
+
+        int i = 0;
+        string ans = "";
+
+        while (num ){
+            if (num >= n[i]) {
+                ans += s[i];
+                num = num - n[i];
+            } else
                 i++;
-            }
         }
-        return str;
+        return ans;
     }
 };
