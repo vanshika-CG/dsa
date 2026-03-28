40993ba05a3421c2dcfb0c64025e14029618c5ab
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-28 11:21:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
+        sort(nums.begin() , nums.end());
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end()); 
+        int n = nums.size();
 
-        for(int i=0; i<n ; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue; // for removing duplicate
-           int j=i+1 , k = n-1;
-           while(j < k){
-            if(nums[i] + nums[j] + nums[k] == 0){
-                ans.push_back({nums[i] , nums[j] , nums[k] });
-                j++;
-                k--;
-                while(j < k && nums[j] == nums[j-1]) j++;
+        for(int i = 0 ; i < n ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1 , k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum > 0) k--;
+                else if(sum < 0) j++;
+                else{
+                    ans.push_back({nums[i] , nums[j] , nums[k]});
+                    j++;
+                    k--;
+                }
             }
-            else if(nums[i] + nums[j] + nums[k] > 0) k--;
-            else j++;
-           }
         }
         return ans;
     }
