62973332f397cd083410466091619d6de1e1b5c9
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-30 22:58:32 +0530

    Time: 7 ms (47.85%), Space: 11.6 MB (36.25%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
         vector<int> freq1(26, 0);
         vector<int> freq2(26, 0);
+        vector<int> ans;
         int n = p.size();
 
-        for (char c : p)
+        for (char c : p) {
             freq1[c - 'a']++;
+        }
 
         for (int i = 0; i < s.size(); i++) {
             freq2[s[i] - 'a']++;
 
-            if (i >= n)
+            if (i >= n) {
                 freq2[s[i - n] - 'a']--;
-
-            if (freq1 == freq2) {
-                ans.push_back(i - n + 1);
             }
+
+            if (freq1 == freq2)
+                ans.push_back(i - n + 1);
         }
         return ans;
     }
