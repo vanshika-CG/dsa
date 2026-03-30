919061651fa875b0d9bee9b9b9180ae2824ed56e
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-30 15:56:08 +0530

    Time: 3 ms (36.63%), Space: 113.8 MB (27.49%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int n = nums.size();
         int sum = 0;
-        int maxsum =0;
+        int maxsum = INT_MIN;
+        int n = nums.size();
 
-        for(int i=0; i<k ; i++){
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
-        }
+            if (i >= k) {
+                sum -= nums[i - k];
+            }
 
-        maxsum = sum;
-
-        for(int i = k; i<n ; i++){
-            sum += nums[i] - nums[i - k];
-            maxsum = max(sum , maxsum);
+            if (i >= k - 1)
+                maxsum = max(sum, maxsum);
         }
-        return (double)maxsum/k;
+        return (double)maxsum / k;
     }
 };
