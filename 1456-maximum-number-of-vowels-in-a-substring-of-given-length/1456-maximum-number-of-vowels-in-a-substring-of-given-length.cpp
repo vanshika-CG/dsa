6d4f150da810531174b51983a06cd3b9e444bb31
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-03-31 11:12:02 +0530

    Time: 5 ms (63.7%), Space: 13.3 MB (27.68%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int ans = 0;
-        int maxans = 0;
+        int n = s.size();
+        int count = 0;
+        int maxcount = 0;
 
-        for (int i = 0; i < k; i++) {
-            if (vowel(s[i]))
-                ans++;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u')
+                count++;
+
+            if (i >= k) {
+                if (s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' ||
+                    s[i - k] == 'o' || s[i - k] == 'u')
+                    count--;
+            }
+
+            maxcount = max(count, maxcount);
         }
-
-        maxans = ans;
-
-        for (int i = k; i < s.size(); i++) {
-            if (vowel(s[i]))
-                ans++;
-            if (vowel(s[i - k]))
-                ans--;
-
-            maxans = max(ans, maxans);
-        }
-
-        return maxans;
-    }
-
-private:
-    bool vowel(char c) {
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-            return true;
-
-        return false;
+        return maxcount;
     }
 };
