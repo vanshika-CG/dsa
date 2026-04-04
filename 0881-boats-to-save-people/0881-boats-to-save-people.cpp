335b3b8c0e873144e2fdead9d72509c85227205a
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-04 11:47:26 +0530

    Time: 17 ms (57.8%), Space: 45.9 MB (37.96%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        int ans = 0;
+        int n = people.size();
         sort(people.begin(), people.end());
+        int l = 0, r = n - 1;
 
-        int i = 0, j = people.size() - 1;
-        int boat = 0;
-
-        while (i <= j) {
-            if (people[i] + people[j] <= limit)
-                i++;
-            j--; // as limit is always small or equal to last element (constrain)
-            boat++;
+        while (l <= r) {
+            if (people[l] + people[r] <= limit)
+                l++;
+            r--;
+            ans++;
         }
-        return boat;
+
+        return ans;
     }
 };
