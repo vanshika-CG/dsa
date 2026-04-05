3531413d88749dd3b4998b074556eda630dfee00
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-05 12:43:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (50.07%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    bool vowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
-    };
-
     string reverseVowels(string s) {
-        int left = 0, right = s.size() - 1;
+        int n = s.size();
+        int l = 0, r = n - 1;
 
-        while (left < right) {
+        while (l < r) {
+            if (!isvowel(s[l]))
+                l++;
+            if (!isvowel(s[r]))
+                r--;
 
-            if (left < right && !vowel(s[left]))
-                left++;
-            if (left < right && !vowel(s[right]))
-                right--;
-
-            swap(s[left], s[right]);
-
-            left++;
-            right--;
+            if (isvowel(s[l]) && isvowel(s[r])) {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
         }
         return s;
+    }
+
+    bool isvowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            return true;
+
+        return false;
     }
 };
