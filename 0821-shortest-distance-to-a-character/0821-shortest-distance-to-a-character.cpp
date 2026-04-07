81d1a42815183ae7b5f927520d5fd8936326c223
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-07 10:35:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.77%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -5,18 +5,19 @@ public:
         vector<int> ans(n);
         int prev = -n;
 
-        // left -> right
         for (int i = 0; i < n; i++) {
-            if (s[i] == c)
+            if (s[i] == c) {
                 prev = i;
+            }
             ans[i] = i - prev;
         }
 
         prev = 2 * n;
-        // right -> left
+
         for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == c)
+            if (s[i] == c) {
                 prev = i;
+            }
             ans[i] = min(ans[i], prev - i);
         }
         return ans;
