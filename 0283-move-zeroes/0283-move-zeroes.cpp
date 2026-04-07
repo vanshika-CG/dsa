45d71eef75db916c55a5e44c316f88f447cd0308
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-07 11:35:30 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int index = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i] != 0) nums[index++] = nums[i];
-        }
+        int idx = 0;
 
-        for(int i=index ; i<n; i++){
-            nums[i] = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[idx]);
+                idx++;
+            }
         }
     }
 };
