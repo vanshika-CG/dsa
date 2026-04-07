fa6a476a26e97d4d3c0ae68b3c0b1a7939aac2ad
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-07 12:03:55 +0530

    Time: 0 ms (100%), Space: 8.8 MB (12.94%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st1, st2;
+        stack<char> s1, s2;
 
         for (char c : s) {
             if (c == '#') {
-                if (!st1.empty())
-                    st1.pop();
+                if (!s1.empty())
+                    s1.pop();
             } else {
-                st1.push(c);
+                s1.push(c);
             }
         }
 
         for (char c : t) {
             if (c == '#') {
-                if (!st2.empty())
-                    st2.pop();
+                if (!s2.empty())
+                    s2.pop();
             } else {
-                st2.push(c);
+                s2.push(c);
             }
         }
-
-        if (st1.size() != st2.size())
-            return false;
-
-        while (!st1.empty()) {
-            if (st1.top() != st2.top())
-                return false;
-            st1.pop();
-            st2.pop();
-        }
-        return true;
+        return s1 == s2;
     }
 };
