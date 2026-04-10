7379ea8ef164f45797b5623d4c551f53bd6b2544
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-10 10:37:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int l = 0;
 
-        for(int i =0; i<nums.size(); i++){
-            if(nums[i] % 2 == 0){
-                swap(nums[i] , nums[l]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                swap(nums[i], nums[l]);
                 l++;
             }
         }
