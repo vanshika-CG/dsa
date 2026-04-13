d1e41c9e060c3804645da8bfac8d8e10f4a879dd
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-13 15:21:00 +0530

    Time: 19 ms (11.74%), Space: 11.1 MB (7.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size();
-        vector<int> freq(26, 0);
+        unordered_map<int, int> m;
         int maxfreq = 0;
-        int len = 0;
-        int j = 0;
+        int i = 0;
+        int maxlen = 0;
 
-        for (int i = 0; i < n; i++) {
-            freq[s[i] - 'A']++;
-            maxfreq = max(maxfreq, freq[s[i] - 'A']);
+        for (int j = 0; j < s.size(); j++) {
+            m[s[j]]++;
+            maxfreq = max(maxfreq, m[s[j]]);
 
-            while ((i - j + 1) - maxfreq > k) {
-                freq[s[j] - 'A']--;
-                j++;
+            while ((j - i + 1) - maxfreq > k) {
+                m[s[i]]--;
+                i++;
             }
-
-            len = max(len, i - j + 1);
+            maxlen = max(maxlen, j - i + 1);
         }
-        return len;
+
+        return maxlen;
     }
 };
