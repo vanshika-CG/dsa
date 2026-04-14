61eee2ef1967d8655e7a8357ed3524e847107311
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-14 11:52:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.68%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 || x==1) return x;
-        int left =1;
-        int right = x;
-        int ans =0;
-        while(left <= right){
-            long mid = left + (right - left)/2;
-            if(mid * mid == x) return mid;
-            else if(mid * mid < x){
-            ans = mid;
-            left = mid + 1;
-            }
-            else right = mid -1;
+        if (x == 0 || x == 1)
+            return x;
+        int l = 0, h = x;
+        int ans = 0;
+
+        while (l <= h) {
+            long mid = l + (h - l) / 2;
+            if (mid * mid == x)
+                return mid;
+            else if (mid * mid < x) {
+                ans = mid;
+                l = mid + 1;
+            } else
+                h = mid - 1;
         }
         return ans;
     }
