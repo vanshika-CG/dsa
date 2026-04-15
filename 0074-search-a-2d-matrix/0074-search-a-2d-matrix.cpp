57250d70804e25423718c5bc98eb53b3fa7c83bf
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-15 19:19:47 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.09%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,35 +3,29 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int l = 0, h = n - 1;
 
-        int srow = 0, erow = n - 1;
-
-        while (srow <= erow) {
-            int mid = srow + (erow - srow) / 2;
-
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
             if (matrix[mid][0] <= target && matrix[mid][m - 1] >= target) {
-                return searchRow(matrix, target, mid);
-            } else if (matrix[mid][0] > target) {
-                erow = mid - 1;
-            } else {
-                srow = mid + 1;
-            }
-        }
-        return false;
-    }
 
-private:
-    bool searchRow(vector<vector<int>>& matrix, int target, int row) {
-        int l = 0, r = matrix[0].size() - 1;
+                int left = 0, right = m - 1;
+                while (left <= right) {
+                    int midd = left + (right - left) / 2;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (matrix[row][mid] == target)
-                return true;
-            else if (matrix[row][mid] < target)
-                l = mid + 1;
+                    if (matrix[mid][midd] == target)
+                        return true;
+                    else if (matrix[mid][midd] > target)
+                        right = midd - 1;
+                    else
+                        left = midd + 1;
+                }
+                return false;
+
+            } else if (matrix[mid][0] > target)
+                h = mid - 1;
             else
-                r = mid - 1;
+                l = mid + 1;
         }
         return false;
     }
