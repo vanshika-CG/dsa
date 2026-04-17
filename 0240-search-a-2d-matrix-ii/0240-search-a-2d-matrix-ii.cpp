f8e3dc7c8ba437ef81448d71317f279c21878a25
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-04-17 10:31:20 +0530

    Time: 53 ms (59.29%), Space: 18.6 MB (92.44%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,18 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int r = m - 1;
-        for (int i = 0; i < n; i++) {
-            int l = 0;
-            while (l <= r) {
-                int mid = l + (r - l) / 2;
-                if (matrix[i][mid] == target)
-                    return true;
-                if (matrix[i][mid] > target) {
-                    r = mid - 1;
-                } else
-                    l = mid + 1;
-            }
+        int r = 0, c = m - 1;
+
+        while (r < n && c >= 0) {
+            if (matrix[r][c] == target)
+                return true;
+            else if (matrix[r][c] > target)
+                c--;
+            else
+                r++;
         }
         return false;
     }
