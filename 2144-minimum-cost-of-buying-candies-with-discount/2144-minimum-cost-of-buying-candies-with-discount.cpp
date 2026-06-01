70854e592dee84f7b41ff1dcda3a76bc6962e11b
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-06-01 12:20:39 +0530

    Time: 3 ms (12.55%), Space: 14.2 MB (49.81%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < cost.size(); i++) {
+            if (i % 3 != 2)
+                ans += cost[i];
+        }
+        return ans;
+    }
+};
