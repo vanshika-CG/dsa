a83c453f9d08189429467f89fb0527ee23c60a8b
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-06-09 13:01:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1 , j = n - 1 , k = m + n -1;
+        int i = m - 1;
+        int j = n - 1;
 
-        while(j >= 0 && i >= 0){
-            if(nums1[i] < nums2[j]){
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
                 nums1[k] = nums2[j];
-                j--;
                 k--;
+                j--;
             } else {
                 nums1[k] = nums1[i];
                 i--;
                 k--;
             }
         }
-        while(j >= 0){
+
+        while (j >= 0) {
             nums1[k] = nums2[j];
-            j--;
             k--;
+            j--;
         }
     }
 };
