c89d068adf0d3a19d2a58f7f71a42068ee71abe9
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-06-11 13:27:32 +0530

    Time: 4 ms (64.79%), Space: 13.6 MB (16.3%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
+        int i = 0;
 
-        for(int i=0; i< n ; i++){
+        while (i < n) {
             ans.push_back(nums[i]);
-            ans.push_back(nums[n+i]);
+            ans.push_back(nums[i + n]);
+            i++;
         }
         return ans;
     }
