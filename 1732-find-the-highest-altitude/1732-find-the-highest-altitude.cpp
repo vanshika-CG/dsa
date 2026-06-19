96fa33200cb1afabca35d9d1249be4a97351847e
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-06-19 23:01:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (94.12%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int> alt;
-        int pre = 0;
-        int great = 0;
+        int ans = 0;
+        int sum = 0;
 
-        for (int i = 0; i < gain.size(); i++) {
-            alt.push_back(pre);
-            pre += gain[i];
+        for (int num : gain) {
+            sum += num;
+            ans = max(sum, ans);
         }
-        alt.push_back(pre);
-
-        for (int num : alt)
-            if (great < num)
-                great = num;
-        return great;
+        return ans;
     }
 };
