79033ad8d2feb9deab5657fb8d81c4443ac50762
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-06-23 19:56:23 +0530

    Time: 2 ms (15.14%), Space: 8.8 MB (67.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == '(' || s[i] =='{' || s[i]== '[') st.push(s[i]);
-            else{
-                if(st.empty()) return false;
-                else if(s[i] == ')' && st.top() =='(') st.pop();
-                else if(s[i] == '}' && st.top() =='{') st.pop();
-                else if(s[i] == ']' && st.top() =='[') st.pop();
-                else return false;
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[')
+                st.push(c);
+            else {
+                if (st.empty())
+                    return false;
+
+                if (c == ')' && st.top() == '(')
+                    st.pop();
+                else if (c == '}' && st.top() == '{')
+                    st.pop();
+                else if (c == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return false;
             }
         }
         return st.empty();
