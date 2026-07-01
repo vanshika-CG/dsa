678de46e8eb3bfc78a5cc80e293ee5d813f0d401
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-07-01 15:00:01 +0530

    Time: 0 ms (100%), Space: 97.2 MB (87.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit =0;
+        int profit = 0;
         int mini = prices[0];
 
-        for(int i=1; i<prices.size(); i++){
-            if(prices[i] < mini) mini = prices[i];
-            profit = max(profit , prices[i]-mini);
+        for (int p : prices) {
+            mini = min(mini, p);
+
+            profit = max(profit, p - mini);
         }
+
         return profit;
     }
 };
