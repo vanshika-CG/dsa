badda70d14cd72e51e3b4e303103a7ec7739536f
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-07-14 13:32:30 +0530

    Time: 3 ms (59.47%), Space: 117.9 MB (82.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,29 +13,27 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
+        ListNode* prev = nullptr;
 
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+        while(fast && fast -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
 
         ListNode* curr = slow;
-        ListNode* prev = nullptr;
 
-        while (curr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
+        while(curr){
+            ListNode* next = curr -> next;
+            curr -> next = prev;
+            prev = curr ;
             curr = next;
         }
 
-        while (prev) {
-            if (prev->val != head->val)
-                return false;
-            prev = prev->next;
-            head = head->next;
+        while(prev){
+            if(prev -> val != head -> val) return false;
+            head = head -> next;
+            prev = prev -> next;
         }
-
         return true;
     }
 };
