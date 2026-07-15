4619a17a7ea3e0e1b4a4255656ba175794a5655c
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-07-15 10:46:25 +0530

    Time: 55 ms (15.79%), Space: 84.7 MB (83.65%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
         unordered_map<int, int> m;
         int j = 0;
         int ans = 0;
 
-        for (int i = 0; i < fruits.size(); i++) {
+        for (int i = 0; i < n; i++) {
             m[fruits[i]]++;
 
             while (m.size() > 2) {
@@ -17,7 +18,6 @@ public:
 
             ans = max(ans, i - j + 1);
         }
-
         return ans;
     }
 };
