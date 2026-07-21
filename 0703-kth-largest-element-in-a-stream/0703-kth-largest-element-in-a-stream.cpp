2b8639b7353d3965ba2002138f49618f708a0316
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-07-21 10:32:53 +0530

    Time: 10 ms (33.56%), Space: 33.1 MB (64%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -2,8 +2,10 @@ class KthLargest {
 public:
     priority_queue<int, vector<int>, greater<int>> pq;
     int K;
+
     KthLargest(int k, vector<int>& nums) {
         K = k;
+
         for (int x : nums) {
             pq.push(x);
 
@@ -14,7 +16,6 @@ public:
 
     int add(int val) {
         pq.push(val);
-
         if (pq.size() > K)
             pq.pop();
 
