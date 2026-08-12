86557b8fbf493f1668ee9265915791f6f0ea58be
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-08-12 10:50:15 +0530

    Time: 4 ms (13.03%), Space: 28.8 MB (94.52%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSame(TreeNode* p, TreeNode* q) {
+        if (!p && !q)
+            return true;
+        if (!p || !q)
+            return false;
+
+        if (p->val != q->val)
+            return false;
+
+        int l = isSame(p->left, q->left);
+        int r = isSame(p->right, q->right);
+
+        return l && r;
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot)
+            return true;
+        if (!root || !subRoot)
+            return false;
+
+        if (root->val == subRoot->val && isSame(root, subRoot))
+            return true;
+
+        int l = isSubtree(root->left, subRoot);
+        int r = isSubtree(root->right, subRoot);
+
+        return l || r;
+    }
+};
