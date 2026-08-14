fec7b58573f40ddbaca5239182bbb970f853e806
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-08-14 10:20:11 +0530

    Time: 0 ms (100%), Space: 23.5 MB (99.84%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,6 +12,8 @@
  */
 class Solution {
 public:
+    int ans = 0;
+
     int height(TreeNode* root) {
         if (!root)
             return 0;
@@ -19,19 +21,13 @@ public:
         int l = height(root->left);
         int r = height(root->right);
 
+        ans = max(ans, l + r);
+
         return max(l, r) + 1;
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-
-        if (!root)
-            return 0;
-
-        int l = diameterOfBinaryTree(root->left);
-        int r = diameterOfBinaryTree(root->right);
-
-        int curr = height(root->left) + height(root->right);
-
-        return max(curr, max(l, r));
+        height(root);
+        return ans;
     }
 };
