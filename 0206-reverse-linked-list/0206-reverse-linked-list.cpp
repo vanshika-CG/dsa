65cb21c21c7ef42a36130494fe04eb746dee6230
VANSHIKA JANGAM <vanshika.jangamcg@gmail.com>
2026-09-23 14:02:27 +0530

    Time: 0 ms (100%), Space: 13.3 MB (71.69%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,15 +11,18 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
+        if (!head)
+            return nullptr;
         ListNode* prev = nullptr;
+        ListNode* curr = head;
 
-        while(curr){
-            ListNode* next = curr -> next;
-            curr -> next = prev ;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
             prev = curr;
             curr = next;
         }
-        return prev ;
+
+        return prev;
     }
 };
